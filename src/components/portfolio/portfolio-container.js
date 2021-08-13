74a5f917ffbd82492800403ead277f251987e5c0
nathenjo <nateselectronicrepair@gmail.com>
2021-08-13 12:32:01 -0400

    Added portfolio item to src directory

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,12 +1,21 @@
 import { directive } from "babel-types";
 import React, { Component } from "react";
 
+import PortfolioItem from "./portfolio-item";
+
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+        console.log("Portfolio container has rendered");
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio Items Go Here...</h2>
+
+                <PortfolioItem />
             </div>
         )
     }
