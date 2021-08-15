6c6c6b45f5d81c75cd6ab6da84e4b8d28ed12921
nathenjo <nateselectronicrepair@gmail.com>
2021-08-15 18:38:11 -0400

    Added navigation links and other components

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from './navigation/navigation-container';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Nathen Johnson Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-        <PortfolioContainer />
+        <Router>
+          <div>
+            <h1>Nathen Johnson Portfolio</h1>
+            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about-me" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
